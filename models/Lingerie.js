71177e56b1803acb8c9cc4a4950f7f5090b450fe
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-12-06 13:12:23 +0100

    bdd2

--- models/Lingerie.js
+++ models/Lingerie.js
@@ -2,60 +2,57 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const lingerieSchema = new Schema({
-    image: {
-        type: String,
-        default:
-          "https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575543690/MEG/1280px-No_image_3x4.svg_bugbjl.png"
-      },    
-    name: String,
-    description: String,
-    type: {
-        type: [String],
-        enum: [
-            "soutien-gorge",
-            "culotte",
-            "maillot de bain",
-            "homewear",
-            "pyjama"
-        ]
-    },
-color: String,
-size: {
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575543690/MEG/1280px-No_image_3x4.svg_bugbjl.png"
+  },
+  name: String,
+  description: String,
+  type: {
+    type: [String],
+    enum: ["soutien-gorge", "culotte", "maillot de bain", "homewear", "pyjama"]
+  },
+  color: String,
+  size: {
     type: [String],
     enum: [
-        "34",
-        "36",
-        "38",
-        "40",
-        "42",
-        "44",
-        "46",
-        "48",
-        "50",
-        "85B",
-        "90B",
-        "95B",
-        "100B",
-        "105B", 
-        "85C",
-        "90C",
-        "95C",
-        "100C",
-        "105C", 
-        "85D",
-        "90D",
-        "95D",
-        "100D",
-        "105D", 
-        "85E",
-        "90E",
-        "95E",
-        "100E",
-        "105E", 
+      "34",
+      "36",
+      "38",
+      "40",
+      "42",
+      "44",
+      "46",
+      "48",
+      "50",
+      "85B",
+      "90B",
+      "95B",
+      "100B",
+      "105B",
+      "85C",
+      "90C",
+      "95C",
+      "100C",
+      "105C",
+      "85D",
+      "90D",
+      "95D",
+      "100D",
+      "105D",
+      "85E",
+      "90E",
+      "95E",
+      "100E",
+      "105E"
     ]
-},
-price: Number,
-quantity: Number,
+  },
+  price: Number,
+  quantity: {
+    type: [String],
+    enum: ["1", "2", "3", "4", "5", "6", "7", "8", "50"]
+  }
 });
 
 const lingerieModel = mongoose.model("lingerie", lingerieSchema);
--- models/Vetement.js
+++ models/Vetement.js
@@ -2,41 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const vetementSchema = new Schema({
-image: {
-        type: String,
-        default:
-          "https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575543690/MEG/1280px-No_image_3x4.svg_bugbjl.png"
-      },    
-    name: String,
-    description: String,
-    type: {
-        type: [String],
-        enum: [
-            "robe",
-            "chemise",
-            "tee-shirt",
-            "pull",
-            "jupe",
-            "pantalon"
-        ]
-    },
-color: String,
-size: {
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575543690/MEG/1280px-No_image_3x4.svg_bugbjl.png"
+  },
+  name: String,
+  description: String,
+  type: {
     type: [String],
-    enum: [
-        "34",
-        "36",
-        "38",
-        "40",
-        "42",
-        "44",
-        "46",
-        "48",
-        "50"
-    ]
-},
-price: Number,
-quantity: Number
+    enum: ["robe", "chemise", "tee-shirt", "pull", "jupe", "pantalon"]
+  },
+  color: String,
+  size: {
+    type: [String],
+    enum: ["34", "36", "38", "40", "42", "44", "46", "48", "50"]
+  },
+  price: Number,
+  quantity: {
+    type: [String],
+    enum: ["1", "2", "3", "4", "5", "6", "7", "8", "50"]
+  }
 });
 
 const vetementModel = mongoose.model("vetement", vetementSchema);
